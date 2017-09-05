a8edf618b12d15c2a832a1a12a881f2710f60348
HKHF\mengtw <mengtw@huntkey.net>
2017-09-05 14:43:30 +0800

    皮肤主题下载

--- flow/src/main.js
+++ flow/src/main.js
@@ -3,10 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< Updated upstream
-=======
+
 import '../theme/index.css'
->>>>>>> Stashed changes
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
--- flow/src/router/index.js
+++ flow/src/router/index.js
@@ -1,35 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< Updated upstream
 import _ from 'lodash'
-import Hello from '@/components/Hello'
 import flowModuleConfigRouter from './flowModuleConfig/flowModuleConfig'
-=======
-import FlowModelDef from '@/components/FlowModelDef'
->>>>>>> Stashed changes
-
-Vue.use(Router)
+import FlowModelDef from '@/components/flowModelDef/FlowModelDef'
 
 Vue.use(Router)
 const rootRouter = [{
   path: '/',
-  name: 'Hello',
-  component: Hello,
-  redirect: '/Hello'
+  name: 'flowModelDef',
+  component: FlowModelDef,
+  redirect: '/flowModelDef'
 }]
 
 var route = _.concat(rootRouter, flowModuleConfigRouter)
 export default new Router({
-<<<<<<< Updated upstream
   routes: route
-=======
-  routes: [
-    {
-      path: '/',
-      name: 'flowModelDef',
-      component: FlowModelDef
-    }
-  ]
->>>>>>> Stashed changes
 })
 
