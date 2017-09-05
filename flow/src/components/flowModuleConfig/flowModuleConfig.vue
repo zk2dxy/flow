40f426b6f0ec307e8567b44c632a224ad4b11dd9
HKHF\weijian <weijian2@huntkey.net>
2017-09-05 14:49:01 +0800

    修改

--- flow/src/components/flowModuleConfig/flowModuleConfig.vue
+++ flow/src/components/flowModuleConfig/flowModuleConfig.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="flowmoduleconfig">
-    <el-row>
-        <el-col :span="24">
-          <span>
-            <i class="sceoIcon sceoIcon-undo2"></i> 返回上一级
-          </span>
-          <span class="demonstration">当前流程</span>
-          当前状态：
-        </el-col>
+  <!-- 头部 -->
+    <el-col :span="24" class="header">
+      <el-row>
+        <span>
+          <i class="el-icon-menu"></i>返回上一级
+        </span>
+        <span>当前流程:</span>
+        当前状态：
         <el-select v-model="value" placeholder="请选择">
           <el-option
             v-for="item in options"
@@ -17,7 +17,11 @@
           </el-option>
         </el-select>
       </el-row>
-    
+    </el-col>
+  <!-- 新增 -->  
+  <div class="addFlowModule"></div>     
+  <!-- 模型列表 -->
+  <div class="flowModuleList"></div> 
   </div>
 </template>
 <script>
@@ -27,23 +31,49 @@
       return {
         options: [{
           value: '1',
-          label: '黄金糕'
+          label: '全部'
         }, {
           value: '2',
-          label: '双皮奶',
-          disabled: true
+          label: '启用'
         }, {
           value: '3',
-          label: '蚵仔煎'
+          label: '停用'
         }, {
           value: '4',
-          label: '龙须面'
-        }, {
-          value: '5',
-          label: '北京烤鸭'
+          label: '新增'
         }],
-        value: ''
+        value: '1'
       }
     }
   }
 </script>
+
+<style scoped>
+  * {
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  }
+
+  html, body {
+    height: 100%;
+    overflow: auto;
+  }
+
+  body {
+    margin: 0;
+    overflow: aoto;
+  }
+
+  div {
+    color: #6B6B74;
+    font-size: 13px;
+  }
+
+  
+  .header{
+    padding: 1rem 1rem 0 1rem;
+      
+  }
+  .header .el-col {
+    height: 28px;
+  } 
+</style>
